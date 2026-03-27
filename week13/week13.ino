90a4504b4cfc5b2a30bcde4aa60ad9c3ad5325a1
teknobun <marta.gaspardelrosal@tuni.fi>
2026-03-27 12:18:19 +0200

    task1 added

--- week13/week13.ino
+++ week13/week13.ino
@@ -1,3 +1,72 @@
+/* TASK 1
+
+#include <TimerOne.h>
+#include <LiquidCrystal.h>
+const int rs = 4, en = 6, d4 = 7, d5 = 8, d6 = 9, d7 = 10;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+const uint8_t buttonPIN = 2;
+
+volatile unsigned long pulseCount = 0;
+unsigned long finalCount = 0;
+float frequency = 0.0;
+volatile bool tenSecondsPassed = false;
+
+volatile unsigned long lastPulseTime = 0;
+volatile unsigned long pulseDurationUs = 0;
+
+
+void pulseISR(){
+  unsigned long  currentTime = micros();
+  if(lastPulseTime > 0){
+    pulseDurationUs = currentTime - lastPulseTime;
+  }
+  lastPulseTime = currentTime;
+  pulseCount++;
+}
+
+void timerISR(){
+  finalCount = pulseCount;
+  pulseCount = 0;
+
+  frequency = (float)finalCount / 1.0;
+  tenSecondsPassed = true;
+}
+
+void setup() {
+  lcd.begin(20, 4); 
+  lcd.clear();
+
+  pinMode(buttonPIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPIN), pulseISR, FALLING);
+
+  Timer1.initialize(1000000); 
+  Timer1.attachInterrupt(timerISR);
+}
+
+void loop() {
+
+    lcd.setCursor(0, 2);
+    lcd.print("Duration: ");
+    lcd.print((float)pulseDurationUs / 1000.0, 1);
+    lcd.print(" ms");
+
+    lcd.setCursor(0, 0);
+    lcd.print("Pulses: ");
+    lcd.print(pulseCount);
+
+  if (tenSecondsPassed) {
+    tenSecondsPassed = false;
+    
+    lcd.clear();
+    
+    lcd.setCursor(0, 1);
+    lcd.print("Freq: ");
+    lcd.print(frequency, 3); 
+    lcd.print(" Hz");
+  }
+}
+*/
+
 // TASK 2
 
 // #include <LiquidCrystal.h>
